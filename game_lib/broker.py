4172127bc68812db0b74f6190d669f822c80d05e
sudyatmikaputra <sudyatmikaputra@gmail.com>
2021-06-22 17:08:40 +0800

    add ping function

--- game_lib/broker.py
+++ game_lib/broker.py
@@ -4,18 +4,30 @@ from game_lib.item import Item
 
 
 class Broker:
-    def __init__(self, name: str = ""):
+    def __init__(self, name: str = ''):
         self.name = name
         self.cash = randint(1500, 2000)
         self.inventory = dict(
-            Silk=Item(cost=randint(250, 300), amount=randint(3, 5)),
-            Silver=Item(cost=randint(450, 600), amount=randint(2, 3)),
-            Gold=Item(cost=randint(750, 1000), amount=randint(1, 2)),
-            Diamond=Item(cost=randint(1800, 2000), amount=randint(0, 1)),
+            Silk=Item(
+                cost=randint(250, 300),
+                amount=randint(3, 5)
+            ),
+            Silver=Item(
+                cost=randint(450, 600),
+                amount=randint(2, 3)
+            ),
+            Gold=Item(
+                cost=randint(750, 1000),
+                amount=randint(1, 2)
+            ),
+            Diamond=Item(
+                cost=randint(1800, 2000),
+                amount=randint(0, 1)
+            )
         )
 
     def __repr__(self) -> str:
-        return pformat(self.__dict__).replace("'", '"')
+        return pformat(self.__dict__).replace('\'', '"')
 
     def sell(self, item_name: str, amount: int) -> (int, bool):
         sell_success = False
@@ -24,7 +36,7 @@ class Broker:
             sell_success = True
             self.inventory[item_name].amount -= amount
             self.cash += total_cost
-
+            
         return total_cost, sell_success
 
     def buy(self, item_name: str, amount: int, total_cost: int) -> str:
@@ -35,24 +47,21 @@ class Broker:
             self.cash -= total_cost
 
         if buy_success:
-            return (
-                buy_success,
-                f"{self.name} has successfully purchased {item_name} for {total_cost:,.2f}!",
-            )
-        return buy_success, f"{self.name} has insufficient funds!"
+            return buy_success, f'{self.name} has successfully purchased {item_name} for {total_cost:,.2f}!'
+        return buy_success, f'{self.name} has insufficient funds!'
 
     def calculate_cost(self, item_name: str, amount: int) -> int:
         return self.inventory[item_name].cost * amount
 
     def refresh(self):
-        self.inventory["Silk"].update_amount(2, 5)
-        self.inventory["Silver"].update_amount(1, 3)
-        self.inventory["Gold"].update_amount(0, 2)
-        self.inventory["Diamond"].update_amount(0, 1)
+        self.inventory['Silk'].update_amount(2, 5)
+        self.inventory['Silver'].update_amount(1, 3)
+        self.inventory['Gold'].update_amount(0, 2)
+        self.inventory['Diamond'].update_amount(0, 1)
         self.cash += randint(-300, 500)
         for item_name in self.inventory.keys():
             self.inventory[item_name].update_cost()
 
 
-if __name__ == "__main__":
-    print(Broker("PlayerA"))
+if __name__ == '__main__':
+    print(Broker('PlayerA'))
--- game_lib/player.py
+++ game_lib/player.py
@@ -10,15 +10,15 @@ class PlayerMenu(ServerProxy):
     def prompt_command(self):
         self.identifier(self.player.name)
         command = self.get_input()
-        while command[0] != "quit":
+        while command[0] != 'quit':
             try:
                 self.execute_command(command)
             except Exception as e:
                 print(e)
             finally:
                 command = self.get_input()
-        end_state = "WIN" if self.player.cash > 20000 else "LOSE"
-        print(f"You have ${self.player.cash:,.2f} remaining. You {end_state}!")
+        end_state = 'WIN' if self.player.cash > 20000 else 'LOSE'
+        print(f'You have ${self.player.cash:,.2f} remaining. You {end_state}!')
 
     def execute_command(self, command):
         if len(command) == 1:
@@ -27,69 +27,58 @@ class PlayerMenu(ServerProxy):
                 self.ping(self.player.name)
         elif len(command) == 2:
             action, obj = command
-            if action == "view":
-                if obj == "broker":
+            if action == 'view':
+                if obj == 'broker':
                     for i, name in enumerate(self.get_broker_list()):
-                        print(f"{i+1}. {name}")
-                elif obj == "inventory":
-                    print(f"{self.player.name}'s inventory:")
+                        print(f'{i+1}. {name}')
+                elif obj == 'inventory':
+                    print(f'{self.player.name}\'s inventory:')
                     for item_name, item_obj in self.player.inventory.items():
                         cost, amount = item_obj.cost, item_obj.amount
                         print(
-                            f"\t{item_name}\tCost: {cost}\tAvailable Amount: {amount}"
-                        )
-                    print(f"Remaining cash: ${self.player.cash:,.2f}")
+                            f'\t{item_name}\tCost: {cost}\tAvailable Amount: {amount}')
+                    print(f'Remaining cash: ${self.player.cash:,.2f}')
                 else:
                     raise self.command_exception()
         elif len(command) == 3:
             action, obj, broker_id = command
-            if action == "view" and obj == "broker":
+            if action == 'view' and obj == 'broker':
                 print(self.preview(self.player.name, broker_id))
             else:
                 raise self.command_exception()
         elif len(command) == 4:
             action, broker_id, item_name, amount = command
             item_name = item_name.capitalize()
-            if action == "buy":
+            if action == 'buy':
                 total_cost, message = self.buy(
-                    self.player.name, broker_id, item_name, amount, self.player.cash
-                )
+                    self.player.name, broker_id, item_name, amount, self.player.cash)
                 if total_cost > 0:
                     self.player.inventory[item_name].amount += amount
                 self.player.cash -= total_cost
                 print(message)
-            elif action == "sell":
+            elif action == 'sell':
                 total_cost = self.player.calculate_cost(item_name, amount)
-                if self.player.inventory[item_name].amount - amount >= 0:
-                    success = self.sell(
-                        self.player.name, broker_id, item_name, amount, total_cost
-                    )
+                if self.player.inventory[item_name].amount - amount>= 0:
+                    success = self.sell(self.player.name, broker_id, item_name, amount, total_cost)
                     if success:
                         self.player.cash += total_cost
                         self.player.inventory[item_name].amount -= amount
-<<<<<<< HEAD
                         message = f'Successfully sell {item_name} to broker {broker_id}. You have ${self.player.cash:,.2f}!'
-=======
-                        message = f"Sucessfully sell {item_name} to broker {broker_id}. You have ${self.player.cash:,.2f}!"
->>>>>>> 1aca520dc3dfbf14dea66adf262a1676d44a0fdf
                     else:
-                        message = f"Broker {broker_id} does not have enough cash!"
+                        message = f'Broker {broker_id} does not have enough cash!'
                     print(message)
-                else:
+                else :
                     print(f"Insufficient item quantity!")
             else:
                 raise self.command_exception()
 
     def command_exception(self):
-        return Exception("Invalid command, please try again!")
+        return Exception('Invalid command, please try again!')
 
     def get_input(self):
-        return [
-            int(x) if x.isdigit() else x
-            for x in input(f"{self.update()} Input command: ").split()
-        ]
+        return [int(x) if x.isdigit() else x for x in input(f'{self.update()} Input command: ').split()]
 
 
-if __name__ == "__main__":
-    with PlayerMenu("a", "http://localhost:7080") as proxy:
+if __name__ == '__main__':
+    with PlayerMenu('a', 'http://localhost:7080') as proxy:
         proxy.prompt_command()
